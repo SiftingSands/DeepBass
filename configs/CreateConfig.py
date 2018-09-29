ed2fbe0aa92665f98ffed300e75f1ac65d3c88a6
Ubuntu <ubuntu@ip-172-31-41-91.us-west-2.compute.internal>
2018-09-29 02:15:31 +0000

    Initial commit of the config file creation script

---
+++ configs/CreateConfig.py
@@ -0,0 +1,19 @@
+import configparser
+
+config = configparser.ConfigParser()
+config['DEFAULT'] = {'SamplingRate': '16000'}
+
+config['XFade Settings'] = {}
+config['XFade Settings']['Style'] = 'LinearFade' #['HannFade', 'LinearFade', 'Extend'])
+config['XFade Settings']['Time'] = '5'
+
+config['IO'] = {}
+config['IO']['FirstSong'] = 'SmallTownBoy.mp3'
+config['IO']['SecondSong'] = 'IMRemix.mp3'
+config['IO']['Load Directory'] = '~/DeepBass/data/raw/Exp1'
+config['IO']['Save Directory'] = '~/home/ubuntu/DeepBass/data/processed/Exp1'
+config['IO']['Save Name'] = 'Exp1'
+config['IO']['Model Weights'] = '~/DeepBass/src/notebooks/wavenet-ckpt/model.ckpt-200000'
+
+with open('config.ini', 'w') as configfile:
+    config.write(configfile)
